4ea103d1d96ab74a25fb40f8fa1d82dc0e3a3ce9
PMP-247 <palesa.magolego@icloud.com>
2026-01-24 17:38:48 +0200

    React App created

---
+++ our-app/utility-tracker/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-3xl font-bold">
+        Utility Tracker
+      </h1>
+      <p className="mt-4 text-gray-600">
+        React + Tailwind setup successful
+      </p>
+    </div>
+  )
+}
+
+export default App
